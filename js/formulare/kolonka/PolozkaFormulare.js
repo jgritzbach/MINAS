d4604eac6d85d1fd1c4adfdef67a3ecd206909a3
Jan Gritzbach <jgritzbach@localhost.localdomain>
2024-03-01 11:59:25 +0100

    Vymodelována základní podoba objektu položky formuláře - tj. objektu, který sdružuje samotný div, vnitřní kolonku i popisek

--- js/formulare/kolonka/PolozkaFormulare.js
+++ js/formulare/kolonka/PolozkaFormulare.js
@@ -4,31 +4,47 @@ class PolozkaFormulare{
     // současně však také může mít přidružený label
     // a také může mít přidruženou nápovědu, kterou lze zobrazit nějakou událostí.
 
-    constructor(kolonka){
+    constructor(obecnyNazev){
 
-        this.kolonka = kolonka  // kolonka je základní prvek položky -> je to to hlavní, bez čeho by nedávala smysl.        
+        // obecnyNazev - to bude představovat část názvu společnou jako pro položku, tak pro kolonku a label
+        // rozdíl pak bude v tom, že položka a kolonka budou mít každá jiné slovo před touto společnou frází
+        // například pokud obecnyNazev bude "nalezitosti-plne-moci"
+        //tak objekt polozkyFormulare uz sam hned vi, ze 
+
+        const idPolozky = 'polozka-' + obecnyNazev
+        const idKolonky = 'kolonka-' + obecnyNazev
+
+        this.polozka = document.getElementById(idPolozky)
+        this.kolonka = document.getElementById(idKolonky)
+        this.popisek = document.querySelector(`label[for="${idKolonky}"]`)      // příslušný label pro kolonku, pokud existuje
 
     }
 
-    get label(){
-        return this._label
+    get polozka(){
+        return this._polozka
     }
 
-    set label(elementLabel){
-        this._label = elementLabel
+    set polozka(div){
+        this._polozka = div
     }
-   
-    
+
+    get kolonka(){
+        return this._kolonka
+    }
+
+    set kolonka(kolonkaElement){
+        this._kolonka = kolonkaElement
+    }
+
+    get popisek(){
+        return this._popisek
+    }
+
+    set popisek(elementLabel){
+        this._popisek = elementLabel
+    }
 
 
-<div class="polozka-formulare" >
-    <div class="radek-formulare">
-        <select id="nalezitosti-formy-podani" class="zaskrtavaci-volba"></select>
-        <label for="nalezitosti-formy-podani" class="label-napoveda" id="zobrazovatel-napovedy">Náležitosti formy podání</label>
-    </div>
-    <div class="oblast-vypisu vypis-ve-formulari" id="zde-napoveda">
-    
-    </div>
-</div>
+
 
 }
