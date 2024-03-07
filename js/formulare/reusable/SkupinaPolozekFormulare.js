5a9f8b19c8bc78db0969478d56cce2511788e057
Jan Gritzbach <jgritzbach@localhost.localdomain>
2024-03-07 09:33:00 +0100

    Vytvořena nová třída - SkupinaPolozekFormulare - reprezentuje určité logické seskupení položek formuláře, kterou je z nějakého důvodu vhodné držet seskupenou pohromadě
    Důležité je, že takováto skupinasi udržujesvůj název, pod kterým je možné ji identifikovat např. může tento název skupiny vystupovat jako "klíč" ve "slovníku (v JS slovníky nejsou, tedy v vlastnosti v objektu)

---
+++ js/formulare/reusable/SkupinaPolozekFormulare.js
@@ -0,0 +1,12 @@
+class SkupinaPolozekFormulare{
+    // Třída reprezentuje určitou logickou skupinu položek formuláře, kterou je z nějakého důvodu vhodné držet seskupenou pohromadě
+
+    constructor(nazevSkupiny, polozky){
+
+            this.nazevSkupiny = nazevSkupiny
+            this.polozky = polozky
+
+    }
+
+
+}
